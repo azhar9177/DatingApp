533c9b8c72316bd547f0acc7807a1470dbc89230
Zaps Senior dev <zapssrdev@outlook.com>
2023-08-04 16:24:08 +0530

    End of Section 8

--- API/Extentions/ApplicationServiceExtentions.cs
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -17,6 +17,8 @@ namespace API.Extentions
             services.AddCors();
 
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
